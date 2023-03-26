051526ce20073d573155ddcd0eeb12424d4464d1
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-03-26 22:35:31 +0800

    menambahkan class SayaTubeVideo
    implementasi class SayaTubeVideo dan methods nya

--- TP_MOD6_1302213037/TP_MOD6_1302213037/Program.cs
+++ TP_MOD6_1302213037/TP_MOD6_1302213037/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class SayaTubeVideo{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title) 
+    { 
+        this.title = title;
+        Random videoID = new Random();
+        this.id = videoID.Next(9999,100000);
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int InputplayCount) 
+    {
+        this.playCount += InputplayCount;
+    }
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video ID: "+id);
+        Console.WriteLine("Video Title: "+title);
+        Console.WriteLine("Video playCount: "+playCount);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo VideoTuto = new SayaTubeVideo("Tutorial Design By Contract - Liyan M. L. WAYAN");
+        VideoTuto.IncreasePlayCount(1969);
+        VideoTuto.PrintVideoDetails();
+    }
+}
