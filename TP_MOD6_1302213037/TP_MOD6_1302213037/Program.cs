963b9aa3ce7c7b0d0458a11755245a318fce2caa
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-03-26 23:38:42 +0800

    implementasi design by contract

--- TP_MOD6_1302213037/TP_MOD6_1302213037/Program.cs
+++ TP_MOD6_1302213037/TP_MOD6_1302213037/Program.cs
@@ -1,19 +1,37 @@
-﻿public class SayaTubeVideo{
+﻿using System.Diagnostics.Contracts;
+
+public class SayaTubeVideo{
     private int id;
     private string title;
     private int playCount;
 
     public SayaTubeVideo(string title) 
-    { 
+    {
+        Contract.Requires(title != null && title.Length <= 100);
+
         this.title = title;
         Random videoID = new Random();
-        this.id = videoID.Next(9999,100000);
+        this.id = videoID.Next(10000,999999);
         this.playCount = 0;
     }
 
     public void IncreasePlayCount(int InputplayCount) 
     {
-        this.playCount += InputplayCount;
+        Contract.Requires(playCount <= 10000000);
+       
+        try
+        {
+            checked
+            {
+                this.playCount += InputplayCount;
+            }
+            
+        }
+        catch (OverflowException e) 
+        {
+            Console.WriteLine(e.Message);
+        }
+        
     }
     public void PrintVideoDetails()
     {
@@ -28,7 +46,10 @@ public class Program
     public static void Main(string[] args)
     {
         SayaTubeVideo VideoTuto = new SayaTubeVideo("Tutorial Design By Contract - Liyan M. L. WAYAN");
-        VideoTuto.IncreasePlayCount(1969);
-        VideoTuto.PrintVideoDetails();
+        for(int i = 0; i <215;  i++)
+        {
+            VideoTuto.IncreasePlayCount(10000000);
+            VideoTuto.PrintVideoDetails();
+        }
     }
 }
